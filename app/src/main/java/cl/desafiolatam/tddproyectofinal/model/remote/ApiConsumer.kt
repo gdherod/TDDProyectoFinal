066b066024e75c1a1c05f061988899ee1fe31c67
Gary Hermosilla <garuxa@gmail.com>
2020-09-19 23:20:52 -0300

    Se añade el archivo ApiConsumer el cuál contiene el la clase RetrofitClient para la base del consumo de Api, también contiene el convertidor Gson para manipular la información Json que se recibira como respuesta. También contiene la interfaz BBAPI para definir las peticiones que se requieren en la BASE_URL definida en la clase RetrofitClient.

---
+++ app/src/main/java/cl/desafiolatam/tddproyectofinal/model/remote/ApiConsumer.kt
@@ -0,0 +1,27 @@
+package cl.desafiolatam.tddproyectofinal.model.remote
+
+import cl.desafiolatam.tddproyectofinal.model.remote.pojo.BBWrapper
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface BBAPI {
+    @GET("/api/characters")
+    fun charactersList(): Call<BBWrapper>
+}
+
+class RetrofitClient {
+    companion object {
+
+        private const val BASE_URL = "https://www.breakingbadapi.com/api/"
+
+        fun retrofitInstance(): BBAPI {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            return retrofit.create(BBAPI::class.java)
+        }
+    }
+}
