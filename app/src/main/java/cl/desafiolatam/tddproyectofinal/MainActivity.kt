3679ddaf4ee34ab8bf51ec434dce5ccaf58ed28e
Gary Hermosilla <garuxa@gmail.com>
2020-09-21 01:01:31 -0300

    Probando el consumo de API con Retrofit, Coroutines y Repository.

--- app/src/main/java/cl/desafiolatam/tddproyectofinal/MainActivity.kt
+++ app/src/main/java/cl/desafiolatam/tddproyectofinal/MainActivity.kt
@@ -2,10 +2,19 @@ package cl.desafiolatam.tddproyectofinal
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import cl.desafiolatam.tddproyectofinal.model.Repository
 
 class MainActivity : AppCompatActivity() {
+
+    val tag = "MainActivity"
+
+    private var repository = Repository()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        repository.loadApidata()
+
     }
 }
