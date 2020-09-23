0b04ee5455270671d8a6b607b7349f8ec00ee2d9
Gary Hermosilla <garuxa@gmail.com>
2020-09-23 16:15:43 -0300

    Creación de la interfaz DAO para realizar acciones sobre la base de datos (Traer datos, insertar datos, etc)

---
+++ app/src/main/java/cl/desafiolatam/tddproyectofinal/model/db/BBDao.kt
@@ -0,0 +1,16 @@
+package cl.desafiolatam.tddproyectofinal.model.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface BBDao {
+    @Query("SELECT * FROM character_table")
+    fun getAllCharacter(): LiveData<List<Character>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAllCharacter(characterList: List<Character>)
+
+    @Delete
+    fun deleteCharacter(character: Character)
+}
