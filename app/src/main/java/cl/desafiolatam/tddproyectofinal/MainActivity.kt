8835409cb20cc5eda9f8c06f62c6bfc60b9bdc94
Gary Hermosilla <garuxa@gmail.com>
2020-09-25 18:15:15 -0300

    Limpieza de MainActivity

--- app/src/main/java/cl/desafiolatam/tddproyectofinal/MainActivity.kt
+++ app/src/main/java/cl/desafiolatam/tddproyectofinal/MainActivity.kt
@@ -2,19 +2,14 @@ package cl.desafiolatam.tddproyectofinal
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import cl.desafiolatam.tddproyectofinal.model.Repository
 
 class MainActivity : AppCompatActivity() {
 
     val tag = "MainActivity"
 
-    private var repository = Repository()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        repository.loadApidata()
 
     }
 }
