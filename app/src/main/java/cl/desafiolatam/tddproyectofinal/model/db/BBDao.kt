7d5492516fcd3fcd73e55d331c66690d06c1db70
Gary Hermosilla <garuxa@gmail.com>
2020-09-25 18:08:52 -0300

    Correccion por modificacion de Entity

--- app/src/main/java/cl/desafiolatam/tddproyectofinal/model/db/BBDao.kt
+++ app/src/main/java/cl/desafiolatam/tddproyectofinal/model/db/BBDao.kt
@@ -1,16 +1,16 @@
 package cl.desafiolatam.tddproyectofinal.model.db
 
 import androidx.lifecycle.LiveData
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface BBDao {
     @Query("SELECT * FROM character_table")
-    fun getAllCharacter(): LiveData<List<Character>>
+    fun getAllCharacter(): LiveData<List<CharacterEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAllCharacter(characterList: List<Character>)
-
-    @Delete
-    fun deleteCharacter(character: Character)
+    suspend fun insertAllCharacters(characterList: List<CharacterEntity>)
 }
