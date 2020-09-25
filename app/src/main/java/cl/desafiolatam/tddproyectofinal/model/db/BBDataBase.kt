0566fd0fd3b364d869b2bee1385dc94f81c38e53
Gary Hermosilla <garuxa@gmail.com>
2020-09-25 18:09:37 -0300

    Correccion por cambio de Entidad, añadido soporte para TypeConverters

--- app/src/main/java/cl/desafiolatam/tddproyectofinal/model/db/BBDataBase.kt
+++ app/src/main/java/cl/desafiolatam/tddproyectofinal/model/db/BBDataBase.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 // Annotates class to be a Room Database with a table (entity) of the Word class
-@Database(entities = [Character::class], version = 1)
+@Database(entities = [CharacterEntity::class], version = 1, exportSchema = false)
+@TypeConverters(StringListConverter::class)
 abstract class BBDataBase : RoomDatabase() {
 
     abstract fun bbDao(): BBDao
