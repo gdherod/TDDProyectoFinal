444a874e3f3b338812f0de1b995be8004acc9689
Gary Hermosilla <garuxa@gmail.com>
2020-09-25 18:08:27 -0300

    Recombrado a CharacterEntity, ademas de corrrecciones y codigo comentado momentaneamente

--- app/src/main/java/cl/desafiolatam/tddproyectofinal/model/db/CharacterEntity.kt
+++ app/src/main/java/cl/desafiolatam/tddproyectofinal/model/db/CharacterEntity.kt
@@ -1,20 +1,19 @@
 package cl.desafiolatam.tddproyectofinal.model.db
 
-import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "character_table")
-data class Character(
-    @PrimaryKey val id: Int,
+data class CharacterEntity(
+    @PrimaryKey val char_id: Int,
     val name: String,
     val birthday: String,
-    @Embedded val occupation: List<String>,
+    val occupation: List<String>,
     val img: String,
     val status: String,
     val nickname: String,
-    @Embedded val appearance: List<Int>,
+    //val appearance: List<Int>,
     val portrayed: String,
-    val category: String,
-    @Embedded val betterCallSaulAppearance: List<Int?>
+    val category: String
+    //val betterCallSaulAppearance: List<Int?>
 )
