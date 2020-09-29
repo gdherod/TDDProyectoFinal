4010ac44be1339e1ffa51841b663467cda6e01f2
Gary Hermosilla <garuxa@gmail.com>
2020-09-28 23:20:40 -0300

    Añadido nuevo Fragmento con diseño básico para mostrar los detalles del personaje seleccionado

---
+++ app/src/main/java/cl/desafiolatam/tddproyectofinal/view/CharacterDetailFragment.kt
@@ -0,0 +1,93 @@
+package cl.desafiolatam.tddproyectofinal.view
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import cl.desafiolatam.tddproyectofinal.R
+import cl.desafiolatam.tddproyectofinal.viewmodel.BBViewModel
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import kotlinx.android.synthetic.main.fragment_character_detail.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [CharacterDetailFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CharacterDetailFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+    private val bbViewModel: BBViewModel by activityViewModels()
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        Log.d("Parametro 1", param1!!)
+        bbViewModel.getCharacterDetail(param1!!).observe(viewLifecycleOwner, {
+            Log.d("Observando Parámetro 1", it.toString())
+        })
+        return inflater.inflate(R.layout.fragment_character_detail, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment CharacterDetailFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            CharacterDetailFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bbViewModel.getCharacterDetail(param1!!).observe(viewLifecycleOwner, {
+            character_detail_name.text = it.name
+            character_detail_birthday.text = it.birthday
+            character_detail_occupation.text = it.occupation.toString()
+            character_detail_status.text = it.status
+            character_detail_nickname.text = it.nickname
+            character_detail_portrayed.text = it.portrayed
+            character_detail_appearances.text = it.category
+            Glide.with(view.context)
+                .load(it.img)
+                .override(1080)
+                .fitCenter()
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(character_detail_img)
+        })
+    }
+}
